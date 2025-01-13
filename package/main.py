6025a168c980644a66a9d79de6c87af7e097d090
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2025-01-13 16:26:57 +0100

    Update main.py

--- package/main.py
+++ package/main.py
@@ -2,7 +2,7 @@
 Apple Imagewriter
 """
 
-from serial import Serial, SEVENBITS, PARITY_NONE, STOPBITS_ONE
+from serial import Serial, SEVENBITS, PARITY_NONE, STOPBITS_ONE, serialutil
 from time import sleep
 from logging import basicConfig, DEBUG, StreamHandler, getLogger
 
@@ -29,15 +29,18 @@ class AppleImageWriter():
 		self.inUse      = False
 	
 	def connect(self) -> None:
-		self.printer = Serial(
-			port=self.serialPort,
-			baudrate=self.baudrate,
-			bytesize=SEVENBITS,
-			parity=PARITY_NONE,
-			stopbits=STOPBITS_ONE,
-			xonxoff=self.useXONXOFF,
-			dsrdtr=self.useDTR
-		)
+		try:
+			self.printer = Serial(
+				port=self.serialPort,
+				baudrate=self.baudRate,
+				bytesize=SEVENBITS,
+				parity=PARITY_NONE,
+				stopbits=STOPBITS_ONE,
+				xonxoff=self.useXONXOFF,
+				dsrdtr=self.useDTR
+			)
+		except serialutil.SerialException as err:
+			self.logger.error(f"Cannot connect to ImageWriter. Error: ${err}")
 	
 	def disconnect(self) -> None:
 		if self.printer:
@@ -63,9 +66,13 @@ class AppleImageWriter():
 		
 		self.inUse = False
 	
+	@property
+	def underline(self):
+		return self._underline
+
 	@underline.setter
 	def underline(self, value: bool) -> None:
-		self.underline = value
+		self._underline = value
 
 		command: str
 		if value:
@@ -74,9 +81,13 @@ class AppleImageWriter():
 			command = b"\x27\x89"
 		self.printer.write(command)
 
+	@property
+	def boldface(self):
+		return self._boldface
+
 	@boldface.setter
 	def boldface(self, value: bool) -> None:
-		self.boldface = value
+		self._boldface = value
 
 		command: str
 		if value:
@@ -85,9 +96,13 @@ class AppleImageWriter():
 			command = b"\x27\x34"
 		self.printer.write(command)
 	
+	@property
+	def headline(self):
+		return self._headline
+	
 	@headline.setter
 	def headline(self, value: bool) -> None:
-		self.headline = value
+		self._headline = value
 
 		command: str
 		if value:
